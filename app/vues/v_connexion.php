179df132b10fc5c3f9f18ab3d3fe55916642da66
François Lachèse <francois.lachese@gmail.com>
2018-04-06 14:38:43 +0200

    Ajout d'icônes sur les boutons de choix visiteur/comptable.

--- app/vues/v_connexion.php
+++ app/vues/v_connexion.php
@@ -20,8 +20,16 @@
         <h2 class="h2 text-center">Bienvenue sur l'Intranet du Laboratoire Galaxy-Swiss Bourdin</h2>
         <p><h4 class="h4 text-center">Quel est votre profil:</h3></p>
         <div class="btn-group btn-group-justified" role="group" aria-label="...">
-          <a class="btn btn-primary btn-lg" href="?uc=connexion&action=demandeConnexionVisiteur" role="button">Visiteur</a>
-          <a class="btn btn-warning btn-lg" href="?uc=connexion&action=demandeConnexionComptable" role="button">Comptable</a>
+          <a class="btn btn-primary btn-lg" href="?uc=connexion&action=demandeConnexionVisiteur" role="button">
+              <span class="glyphicon glyphicon-briefcase"></span>
+              <br>
+              Visiteur
+          </a>
+          <a class="btn btn-warning btn-lg" href="?uc=connexion&action=demandeConnexionComptable" role="button">
+              <span class="glyphicon glyphicon-folder-open"></span>
+              <br>
+              Comptable
+          </a>
         </div>
       </div>
     </div>
