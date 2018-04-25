e1a141efee905fbf73b27bba30a8aeff97449f48
François Lachèse <francois.lachese@gmail.com>
2018-04-25 09:37:35 +0200

    Ajout de l'onglet comptable Suivre le paiement des fiches de frais

---
+++ app/vues/v_suivrePaiement.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Gestion des frais
+ *
+ * PHP Version 7
+ *
+ * @category  PPE
+ * @package   GSB
+ * @author    François Lachèse
+ * @version   GIT: <0>
+ * @link      https://github.com/FLchs/GSB-AppliMVC
+ */
+?>
+
+<div class="row">
+    <div class="col-md-12">
+        <h1 class="yellow">Valider la ficher des frais</h1>
+    </div>
+</div>
+
+
+<div class="panel panel-primary">
+    <div class="panel-heading">Fiche de frais du mois
+        <?php echo $numMois . '-' . $numAnnee ?> : </div>
+    <div class="panel-body">
+        <strong><u>Etat :</u></strong> <?php echo $libEtat ?>
+        depuis le <?php echo $dateModif ?> <br>
+        <strong><u>Montant validé :</u></strong> <?php echo $montantValide ?>
+    </div>
+</div>
+<div class="panel panel-info">
+    <div class="panel-heading">Eléments forfaitisés</div>
+    <table class="table table-bordered table-responsive">
+        <tr>
+            <?php
+            foreach ($lesFraisForfait as $unFraisForfait) {
+                $libelle = $unFraisForfait['libelle']; ?>
+                <th> <?php echo htmlspecialchars($libelle) ?></th>
+                <?php
+            }
+            ?>
+        </tr>
+        <tr>
+            <?php
+            foreach ($lesFraisForfait as $unFraisForfait) {
+                $quantite = $unFraisForfait['quantite']; ?>
+                <td class="qteForfait"><?php echo $quantite ?> </td>
+                <?php
+            }
+            ?>
+        </tr>
+    </table>
+</div>
+<div class="panel panel-info">
+    <div class="panel-heading">Descriptif des éléments hors forfait -
+        <?php echo $nbJustificatifs ?> justificatifs reçus</div>
+    <table class="table table-bordered table-responsive">
+        <tr>
+            <th class="date">Date</th>
+            <th class="libelle">Libellé</th>
+            <th class='montant'>Montant</th>
+        </tr>
+        <?php
+        foreach ($lesFraisHorsForfait as $unFraisHorsForfait) {
+            $date = $unFraisHorsForfait['date'];
+            $libelle = htmlspecialchars($unFraisHorsForfait['libelle']);
+            $montant = $unFraisHorsForfait['montant']; ?>
+            <tr>
+                <td><?php echo $date ?></td>
+                <td><?php echo $libelle ?></td>
+                <td><?php echo $montant ?></td>
+            </tr>
+            <?php
+        }
+        ?>
+    </table>
+</div>
+
+
+
+<div class="row">
+    <form action="./?uc=suivrepaiement&visiteur=<?php echo($idVisiteur);?>&mois=<?php echo($leMois);?>&action=mettreEnPaiement"
+    method="post" role="form">
+    <input type="hidden" name="uc" value="validerfichefrais">
+        <div class="form-group col-md-12">
+            <button class="btn btn-success" type="submit">Valider</button>
+        </div>
+    </form>
+</div>
